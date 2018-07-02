30ee42b232ad653fe0c7e84b6861d85af5e9f59f
RSMC9ULLSWY2H43\Administrator <1060591385@qq.com>
2018-07-02 19:49:08 +0800

    添加用户名为空和密码为空的测试脚本

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -14,38 +14,38 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    @pytest.mark.parametrize("args", analyze_data("test_login"))
-    def test_login(self, args):
-        username = args["username"]
-        password = args["password"]
-        expect = args["expect"]
-
-        # 点击我的按钮
-        self.page.home.click_mine()
-        # 点击登录/注册
-        self.page.mine.click_login_signup()
-        # 输入用户名和密码,点击登录
-        self.page.login.input_username(username)
-        self.page.login.input_password(password)
-        self.page.login.click_login()
-        # 判断toast是否和数据中的expect一致
-        assert self.page.login.is_toast_exist(expect)
-
-    # @pytest.mark.parametrize("args", analyze_data("test_login_miss_part"))
-    # def test_login_miss_part(self, args):
+    # @pytest.mark.parametrize("args", analyze_data("test_login"))
+    # def test_login(self, args):
     #     username = args["username"]
     #     password = args["password"]
+    #     expect = args["expect"]
     #
-    #     # 主页点击我的
+    #     # 点击我的按钮
     #     self.page.home.click_mine()
-    #     # 我的点击登录
+    #     # 点击登录/注册
     #     self.page.mine.click_login_signup()
-    #
-    #     if not username == "":
-    #         self.page.login.input_username(username)
-    #     if not password == "":
-    #         self.page.login.input_password(password)
-    #     assert not self.page.login.is_login_button_enabled()
+    #     # 输入用户名和密码,点击登录
+    #     self.page.login.input_username(username)
+    #     self.page.login.input_password(password)
+    #     self.page.login.click_login()
+    #     # 判断toast是否和数据中的expect一致
+    #     assert self.page.login.is_toast_exist(expect)
+
+    @pytest.mark.parametrize("args", analyze_data("test_login_miss_part"))
+    def test_login_miss_part(self, args):
+        username = args["username"]
+        password = args["password"]
+
+        # 主页点击我的
+        self.page.home.click_mine()
+        # 我的点击登录
+        self.page.mine.click_login_signup()
+
+        if not username == "":
+            self.page.login.input_username(username)
+        if not password == "":
+            self.page.login.input_password(password)
+        assert not self.page.login.is_login_button_enabled()
 
     # def test_show_password(self):
     #     password = "xxxx"
